df6ad2b73ef15effaf91c0b704c9df2dcc489f45
1214155431lj <1159662451@qq.com>
2019-09-19 18:04:35 +0800

    add page

---
+++ src/components/navBar.vue
@@ -0,0 +1,98 @@
+<template>
+	<div class="navBar">
+		<ul class="nav">
+	    <li v-for="(item,index)  in navArr" :style="item.hide?'display:none':''">
+	    	<div @click="goNav(item.url,index)" :class="{active:index===page}" >
+	      <!-- <router-link :to="{path:item.url}"> -->
+	        <i class="iconfont" :class="item.iconClass"></i>
+	        <span>{{ item.title }}</span>
+	      <!-- </router-link> -->
+	      </div>
+	    </li>
+	  </ul>
+	</div>
+</template>
+<script>
+export default {
+  name: 'navBar',
+  data () {
+    return {
+    	navArr:[
+    		{
+    			title: "推广",
+    		 	url: "/index",
+    		 	iconClass: "icon-tuiguang"
+    		},
+    		{
+    			title: "发现",
+    		 	url: "/members",
+    		 	iconClass: "icon-faxian"
+    		},
+    		{
+    			title: "订单",
+    		 	url: "/visitors",
+    		 	iconClass: "icon-dingdan"
+    		},
+    		{
+    			title: "我的",
+    		 	url: "/mine",
+    		 	iconClass: "icon-wode"
+    		},
+    	]
+    }
+  },
+  props:['page'],
+  mounted(){
+    // let role = localStorage.getItem("role")
+    // if(role == 'property'){
+    //   this.navArr = this.arr2;
+    // }else if(role == 'owner'){
+    //   this.navArr = this.arr;
+    // }else{
+    //   this.navArr = this.arr;
+    // }
+  },
+  methods:{
+  	goNav(url,index){
+  		if(index == this.page){
+  			return;
+  		}else{
+  			this.$router.push({path:''+url});
+  		}  		
+  	}
+  }
+}
+</script>
+<style lang="stylus" type="text/stylus" scoped>
+  .navBar
+    width 100%    
+    padding-top 1rem
+		.nav
+			width 100%
+			height 1rem
+			// border-top 1px solid #ccc
+			box-shadow :1px 4px 5px #666
+			background-color #f5f5f5
+			position fixed
+			left 0px
+			bottom 0px
+			display flex
+			z-index 99
+			li
+				display flex
+				flex 1          
+				div
+					display flex
+					flex 1
+					flex-direction column
+					align-items center
+					justify-content center
+					i
+						font-size .4rem
+					span
+						font-size .2rem
+						margin-top .08rem									
+				.active
+					color #4978c4
+  				
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,54 +7,222 @@
 					<i class="iconfont icon-sousuo"></i>
         	<!-- <i class="iconfont icon-xiaoxi"></i> -->
 				</div>
-	      <tab  class="index-tab" :line-width=2 active-color='#fc378c' v-model="index">
+	      <tab  class="index-tab" :line-width=2 active-color='#fc378c' v-model="selected_index">
 	        <tab-item class="vux-center" :selected="demo2 === item" v-for="(item, index1) in list2" @click="demo2 = item" :key="index1">{{item}}</tab-item>
 	      </tab>
-	      <swiper v-model="index" height="100px" :show-dots="false">
-	        <swiper-item>
+	      <swiper v-model="selected_index" height="100%"  :show-dots="false">
+	        <swiper-item :style='selected_index==0?{position: "static"}:""'>
 	        	<div class="loan-page">
-							<marquee>
-			          <marquee-item v-for="i in 2" :key="i" @click.native="goNotice(i)">
-			          	<i class="iconfont icon-faxian"></i>
-			          	<i class="iconfont icon-wode"></i>
-			          	<i class="iconfont icon-tuiguang"></i>
-			          	<i class="iconfont icon-dingdan"></i>
-			          	<!-- <i class="iconfont">&#xe651;</i>
-			          	<i class="iconfont">&#xe62c;</i>
-			          	<i class="iconfont">&#xe746;</i>
-			          	<i class="iconfont">&#xe604;</i> -->
-			          	<p class="line-ellipsis02">{{(i)}}.大家好！由于节能施工改造，</p>
-			          </marquee-item>
-			        </marquee>
+	        		<div class="loan-notice">
+	        			<marquee>
+				          <marquee-item v-for="i in 2" :key="i" @click.native="goNotice(i)">
+				          	<p class="line-ellipsis02">{{(i)}}.大家好！由于节能施工改造，</p>
+				          </marquee-item>
+				        </marquee>
+	        		</div>								
 			        <div class="product-items">
-			        	<div class="loan-item">
-			        		
+			        	<div class="loan-item clearfix">
+			        		<div class="loan-item-top">
+			        			<img src="../assets/images/cp01.png">
+			        			<h4>xxx卡</h4>
+			        			<p class="loan-date">每周五结</p>
+			        			<p class="loan-rate">月息0.03%</p>
+			        		</div>
+			        		<div class="loan-item-mid">
+			        			<div class="loan-item-limit">
+			        				<b>3000-8000</b><span>额度（元）</span>
+			        			</div>
+			        			<div class="loan-item-vip flex">
+			        				<i class="iconfont icon-vip"></i><span>返<b>2.3%</b></span>
+			        			</div>
+			        		</div>
+			        		<div class="loan-item-bottom">
+			        			<p>普通用户&nbsp;返<span>1.84%</span></p>
+			        		</div>
+			        	</div>
+			        	<div class="loan-item clearfix">
+			        		<div class="loan-item-top">
+			        			<img src="../assets/images/cp01.png">
+			        			<h4>xxx卡</h4>
+			        			<p class="loan-date">每周五结</p>
+			        			<p class="loan-rate">月息0.03%</p>
+			        		</div>
+			        		<div class="loan-item-mid">
+			        			<div class="loan-item-limit">
+			        				<b>3000-8000</b><span>额度（元）</span>
+			        			</div>
+			        			<div class="loan-item-vip flex">
+			        				<i class="iconfont icon-vip"></i><span>返<b>2.3%</b></span>
+			        			</div>
+			        		</div>
+			        		<div class="loan-item-bottom">
+			        			<p>普通用户&nbsp;返<span>1.84%</span></p>
+			        		</div>
+			        	</div>
+			        	<div class="loan-item clearfix">
+			        		<div class="loan-item-top">
+			        			<img src="../assets/images/cp01.png">
+			        			<h4>xxx卡</h4>
+			        			<p class="loan-date">每周五结</p>
+			        			<p class="loan-rate">月息0.03%</p>
+			        		</div>
+			        		<div class="loan-item-mid">
+			        			<div class="loan-item-limit">
+			        				<b>3000-8000</b><span>额度（元）</span>
+			        			</div>
+			        			<div class="loan-item-vip flex">
+			        				<i class="iconfont icon-vip"></i><span>返<b>2.3%</b></span>
+			        			</div>
+			        		</div>
+			        		<div class="loan-item-bottom">
+			        			<p>普通用户&nbsp;返<span>1.84%</span></p>
+			        		</div>
+			        	</div>
+			        	<div class="loan-item clearfix">
+			        		<div class="loan-item-top">
+			        			<img src="../assets/images/cp01.png">
+			        			<h4>xxx卡</h4>
+			        			<p class="loan-date">每周五结</p>
+			        			<p class="loan-rate">月息0.03%</p>
+			        		</div>
+			        		<div class="loan-item-mid">
+			        			<div class="loan-item-limit">
+			        				<b>3000-8000</b><span>额度（元）</span>
+			        			</div>
+			        			<div class="loan-item-vip flex">
+			        				<i class="iconfont icon-vip"></i><span>返<b>2.3%</b></span>
+			        			</div>
+			        		</div>
+			        		<div class="loan-item-bottom">
+			        			<p>普通用户&nbsp;返<span>1.84%</span></p>
+			        		</div>
+			        	</div>
+			        	<div class="loan-item clearfix">
+			        		<div class="loan-item-top">
+			        			<img src="../assets/images/cp01.png">
+			        			<h4>xxx卡</h4>
+			        			<p class="loan-date">每周五结</p>
+			        			<p class="loan-rate">月息0.03%</p>
+			        		</div>
+			        		<div class="loan-item-mid">
+			        			<div class="loan-item-limit">
+			        				<b>3000-8000</b><span>额度（元）</span>
+			        			</div>
+			        			<div class="loan-item-vip flex">
+			        				<i class="iconfont icon-vip"></i><span>返<b>2.3%</b></span>
+			        			</div>
+			        		</div>
+			        		<div class="loan-item-bottom">
+			        			<p>普通用户&nbsp;返<span>1.84%</span></p>
+			        		</div>
+			        	</div>
+			        	<div class="loan-item clearfix">
+			        		<div class="loan-item-top">
+			        			<img src="../assets/images/cp01.png">
+			        			<h4>xxx卡</h4>
+			        			<p class="loan-date">每周五结</p>
+			        			<p class="loan-rate">月息0.03%</p>
+			        		</div>
+			        		<div class="loan-item-mid">
+			        			<div class="loan-item-limit">
+			        				<b>3000-8000</b><span>额度（元）</span>
+			        			</div>
+			        			<div class="loan-item-vip flex">
+			        				<i class="iconfont icon-vip"></i><span>返<b>2.3%</b></span>
+			        			</div>
+			        		</div>
+			        		<div class="loan-item-bottom">
+			        			<p>普通用户&nbsp;返<span>1.84%</span></p>
+			        		</div>
+			        	</div>
+			        	<div class="loan-item clearfix">
+			        		<div class="loan-item-top">
+			        			<img src="../assets/images/cp01.png">
+			        			<h4>xxx卡</h4>
+			        			<p class="loan-date">每周五结</p>
+			        			<p class="loan-rate">月息0.03%</p>
+			        		</div>
+			        		<div class="loan-item-mid">
+			        			<div class="loan-item-limit">
+			        				<b>3000-8000</b><span>额度（元）</span>
+			        			</div>
+			        			<div class="loan-item-vip flex">
+			        				<i class="iconfont icon-vip"></i><span>返<b>2.3%</b></span>
+			        			</div>
+			        		</div>
+			        		<div class="loan-item-bottom">
+			        			<p>普通用户&nbsp;返<span>1.84%</span></p>
+			        		</div>
+			        	</div>
+			        	<div class="loan-item clearfix">
+			        		<div class="loan-item-top">
+			        			<img src="../assets/images/cp01.png">
+			        			<h4>xxx卡</h4>
+			        			<p class="loan-date">每周五结</p>
+			        			<p class="loan-rate">月息0.03%</p>
+			        		</div>
+			        		<div class="loan-item-mid">
+			        			<div class="loan-item-limit">
+			        				<b>3000-8000</b><span>额度（元）</span>
+			        			</div>
+			        			<div class="loan-item-vip flex">
+			        				<i class="iconfont icon-vip"></i><span>返<b>2.3%</b></span>
+			        			</div>
+			        		</div>
+			        		<div class="loan-item-bottom">
+			        			<p>普通用户&nbsp;返<span>1.84%</span></p>
+			        		</div>
 			        	</div>
 			        </div>
 	        	</div>
 	        </swiper-item>
-	        <swiper-item>
+	        <swiper-item :style='selected_index==1?{position: "static"}:""'>
 	        	<div class="">
-	        		
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
 	        	</div>
 	        </swiper-item>
-	        <swiper-item>
-	        	<div>33</div>
+	        <swiper-item :style='selected_index==2?{position: "static"}:""'>
+	        	<div class="">
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        	</div>
+	        </swiper-item :style='selected_index==3?{position: "static"}:""'>
+	        <swiper-item :style='selected_index==4?{position: "static"}:""'>
+	        	<div class="">
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        	</div>
 	        </swiper-item>
-	        <swiper-item>
-	        	<div>44</div>
-	        </swiper-item>
-	        <swiper-item>
-	        	<div>55</div>
+	        <swiper-item :style='selected_index==5?{position: "static"}:""'>
+	        	<div class="">
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        		<p>31313</p>
+	        	</div>
 	        </swiper-item>
 	      </swiper>
 	    </div>
 		</div>
+		<!-- navbar -->
+		<nav-bar :page="0"></nav-bar>	
 	</div>
 </template>
 
 <script>
-import { Tab, TabItem, Swiper, SwiperItem,Marquee, MarqueeItem } from 'vux'
+import { Tab, TabItem, Swiper, SwiperItem,Marquee, MarqueeItem } from 'vux';
+import navBar from "components/navBar";
 const list = () => ['精选', '美食', '电影', '酒店', '外卖'];
 const test = () => ['11', '22', '33', '44', '55'];
 
@@ -66,7 +234,7 @@ export default {
       list1: test(),
       list2: list(),
       demo2: '精选',
-      index: 0,
+      selected_index: 0,
     };
   },
   mounted(){
@@ -76,6 +244,7 @@ export default {
 
   },
   components:{
+  	navBar,
   	Tab,
     TabItem,
     Swiper,
@@ -104,10 +273,87 @@ export default {
 		cursor:pointer;
 		font-size: .36rem;
 	}
+	.loan-page {
+		min-height: 11rem;
+	}
 	.loan-page i{
 		font-size: .4rem;
 		/*color:#48cfad;*/
 		color: #4978c4;
+	}
+	.loan-notice{
+		padding: .2rem 0;
+	}
+	.loan-item{
+		width: 100%;
+		background: #fff;
+		border-radius: .1rem;
+		overflow: hidden;
+		padding: .3rem .2rem .2rem .2rem;
+		margin-bottom: .2rem;
+	}
+	.loan-item-top{
+		display:flex;
+		align-items: center;
+	}
+	.loan-item-top img{
+		width: .4rem;
+	}
+	.loan-item-top h4{
+		font-size: .3rem;
+		margin-left: .1rem;
+
+	}
+	.loan-item-top p.loan-date{
+		background: #f45c5c;
+		color: #fff;
+		font-size: .28rem;
+		padding: .1rem .14rem;
+		border-radius: .06rem;
+		margin: 0 .1rem;
+	}
+	.loan-item-top p.loan-rate{
+		color: #f45c5c;
+		font-size: .28rem;
+		border:1px solid #f45c5c;
+		padding: .08rem .12rem;
+		border-radius: .06rem;
+	}
+	.loan-item-mid{
+		display: flex;
+		-ms-align-items: center;
+		align-items: center;
+		justify-content: space-between;
+		padding: .3rem 0;
+	}
+	.loan-item-limit b{
+		font-size: .4rem;
+		margin-right: .1rem;
+	}
+	.loan-item-limit span{
+		font-size: .28rem;
+		color: #999;
+	}
+	.loan-item-vip i{
+		font-size: .6rem;
+		color:#f45c5c;
+		margin:.2rem .1rem 0 0;
+	}
+	.loan-item-vip span{
+		color:#f45c5c;
+	}
+	.loan-item-vip span b{
+		font-size: .4rem;
+		margin-left: .04rem;
+	}
+	.loan-item-bottom{}
+	.loan-item-bottom p{
+		float: right;
+		color: #999;
+		font-size: .3rem;
+	}
+	.loan-item-bottom p span{
+		color: #333;
 	}
 </style>
 
