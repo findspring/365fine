987538cac90fd29f753e5201d543b7e3ce3fdc64
1214155431lj <1159662451@qq.com>
2019-11-22 15:18:33 +0800

    change

--- src/pages/reapply.vue
+++ src/pages/reapply.vue
@@ -29,6 +29,19 @@
 			    left-icon="phone-o"
 			    placeholder="请输入手机号"
 			  />
+			  <van-field
+			    v-model="sms"
+			    required
+			    center
+			    left-icon="comment-o"
+			    type="number"
+			    maxlength="6"
+			    clearable
+			    placeholder="请输入短信验证码"
+			  >
+			    <van-button color="#1a91eb" slot="button" size="small" type="primary" @click="sendCode" v-if="codeStatus">发送验证码</van-button>
+			    <van-button color="#1a91eb" disabled slot="button" size="small" type="primary" @click="sendCode" v-else>{{time}}s</van-button>
+			  </van-field>
 			</van-cell-group>
 			<div class="apply-rule flex">
 				<van-checkbox v-model="checked" icon-size="16px"></van-checkbox>
@@ -54,6 +67,9 @@ export default {
 			mobile:'',
 			checked:true,
 			allChecked:false,
+			sms:'',
+			codeStatus:true,
+      time:60,
     };
   },
   mounted(){
@@ -62,18 +78,45 @@ export default {
   	goPath(val){
 			this.$router.push({path:val})
 		},
+		sendCode(){
+			let mobileRule = /^[1][3456789][0-9]{9}$/;
+			if(this.mobile && mobileRule.test(this.mobile)){
+				this.codeStatus = false;
+	      let timer = setInterval(() => {
+	        if (this.time > 1) {
+	          this.time--;
+	        }else{
+	          this.codeStatus = true;
+	          this.time = 60;
+	          clearInterval(timer);
+	        }
+	      }, 1000)
+				this.$http({
+	        method: "get",
+	        url: "/user/verification_code/send?username=" + this.mobile,
+	      }).then((res) => {
+
+	      }).catch((err) => {});
+			}else{
+				this.$toast({position:'top',type:'error',message:'请输入正确手机号',duration: 500,});
+			}				
+		},
 		apply(){
 			if(!this.checked){
 				this.$toast({position:'top',message:'请选择已阅读服务协议',duration: 1000,});
 				return false;
 			}
 			let mobileRule = /^[1][3456789][0-9]{9}$/;
-			if(this.realname && this.idCard && this.mobile){
+			if(this.realname && this.idCard && this.mobile && this.sms){
 				if(!mobileRule.test(this.mobile)){
 					this.$toast({position:'top',type:'error',message:'请输入正确手机号',duration: 500,});
 				}else{
-					this.checkRealName(this.realname,this.idCard);
-				}				
+					if(this.sms.length == 6){
+						this.checkRealName(this.realname,this.idCard);
+					}else{
+						this.$toast({position:'top',type:'error',message:'请输入6位验证码',duration: 500,});
+					}						
+				}	
 			}else{
 				this.$toast({position:'top',type:'error',message:'请输入完整信息',duration: 500,});
 			}
@@ -100,33 +143,34 @@ export default {
 				
 		},
 		checkRealName(realName,idCard){
+			// this.$http({
+   //      method: "get",
+   //      url: "/wechat/spread/realnametest?realname="+realName+'&idcard='+idCard,
+   //    }).then((res) => {
+      	// if(res.data.code == 200){
+  		let params = {};
+			params.id = this.$route.query.id;
+			params.realname = this.realname;
+			params.idcard = this.idCard;
+			params.mobile = this.mobile;
+			params.verification_code = this.sms;
+			params.invite_id = this.$route.query.invite_id
 			this.$http({
-        method: "get",
-        url: "/wechat/spread/realnametest?realname="+realName+'&idcard='+idCard,
+        method: "post",
+        url: "/wechat/spread/sharecardapply",
+        data: this.$qs.stringify(params)
       }).then((res) => {
-      	if(res.data.code == 200){
-      		let params = {};
-					params.id = this.$route.query.id;
-					params.realname = this.realname;
-					params.idcard = this.idCard;
-					params.mobile = this.mobile;
-					params.Invite_user_id = this.$route.query.invite_id
-					this.$http({
-		        method: "post",
-		        url: "/wechat/creditcard/cardapply",
-		        data: this.$qs.stringify(params)
-		      }).then((res) => {
-		        let result = res.data.data;
-		        this.$toast({type:'success',message:'申请成功！',duration: 1000,});
-		        setTimeout(() => {
-		          this.$router.push({path:'recommond',query:{id:this.$route.query.id,invite_id:this.$route.query.invite_id}})
-		        }, 1000)
-		      }).catch((err) => {});
-	        let result = res.data.data; 
-      	}else{
-					this.$toast({position:'top',type:'error',message:'请输入正确的身份信息',duration: 500,});
-      	}	      	       
+        let result = res.data.data;
+        this.$toast({type:'success',message:'申请成功！',duration: 1000,});
+        setTimeout(() => {
+          this.$router.push({path:'recommond',query:{id:this.$route.query.id,invite_id:this.$route.query.invite_id}})
+        }, 1000)
       }).catch((err) => {});
+	        // let result = res.data.data; 
+     //  	}else{
+					// this.$toast({position:'top',type:'error',message:'请输入正确的身份信息',duration: 500,});
+     //  	}	      	       
+      // }).catch((err) => {});
 
 		}
   },
